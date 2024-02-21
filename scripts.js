55c292fa55115ee883c1b5de5e8fb48d4ef79e41
Luis Sperotto <luissperottoeu@gmail.com>
2024-02-21 20:26:52 -0300

    segundo commit

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
     $(document).ready(function(){
 
+        const baseImagens = {
+            todas:['imagem.jpg', 'animal.jpg', 'natureza.jpg', 'cidade.jpg'],
+            animais:['animal.jpg', 'animal1.jpg', 'animal2.jpg', 'animal3.jpg'],
+            natureza:['natureza.jpg', 'natureza1.jpg', 'natureza2.jpg', 'natureza3.jpg'],
+            cidade:['cidade.jpg', 'cidade1.jpg', 'cidade2.jpg', 'cidade3.jpg'],
+        }
+    
+        function carregaImagens(){
+            console.log(baseImagens);
+            alert(categoria);
+        }
+
         $('.botao-categoria').on('click',function(){
-            alert(123);
+            $('body').find('.botao-categoria').removeClass(''active');
+            $(this).addclass('active');
+            carregaImagens(categoria);
+            
         });
 
     });
